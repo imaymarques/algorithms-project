0c074df9a3b4c9fd6c7ca42039ebcbde70e44172
imaymarques <mayaramarquescosta@outlook.com>
2023-04-26 13:16:23 -0300

    chore: Impplementing find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums = sorted(nums)
+
+    for index in range(1, len(nums)):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+
+    return False
