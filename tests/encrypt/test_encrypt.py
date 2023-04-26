1e48446ca60605a2f0796dd0bddc4515e5663ce9
imaymarques <mayaramarquescosta@outlook.com>
2023-04-26 11:46:40 -0300

    test: Implement test_encrypt_message test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    msg_error = "tipo inválido para message"
+    key_error = "tipo inválido para key"
+
+    assert encrypt_message(message="abcde", key=2) == "edc_ba"
+    assert encrypt_message(message="abcde", key=3) == "cba_ed"
+    assert encrypt_message(message="abcde", key=5) == "edcba"
+    assert encrypt_message("edcba", 5) != "edcba"
+
+    with pytest.raises(TypeError, match=key_error):
+        encrypt_message("abcc", "abcde")
+
+    with pytest.raises(TypeError, match=msg_error):
+        encrypt_message(2, 2)
