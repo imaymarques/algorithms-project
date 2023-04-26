ca6de7ad0553a7d98e19fd3061b65c86e29b7bdc
imaymarques <mayaramarquescosta@outlook.com>
2023-04-26 11:31:38 -0300

    chore: Implementing study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    counter = 0
+    if not all(
+        isinstance(s, int) and isinstance(e, int)
+        for s, e in permanence_period
+    ):
+        return None
+
+    for s, e in permanence_period:
+        if s <= target_time <= e:
+            counter += 1
+    return counter
